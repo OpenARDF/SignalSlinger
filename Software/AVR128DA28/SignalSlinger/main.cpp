7dcf6cbb4509ee7d23ea7d3054fbecd9b779211b
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2025-08-02 10:22:49 -0500

    Ver 0.72
    o After a successful cloning, while the “success” blink pattern is blinking, a keypress will now reset the success state and return a target device to normal operation without causing transmissions to start.
    
    o Charging from the external battery now works when the BAT X 1 or BAT X 2 command is in effect.
    
    o The BAT X 2 command shuts off radio transmissions and causes warning messages to appear on serial port communications.
    
    o The BAT T float command will now set the threshold value at which the green LED blinks to indicate a low internal battery; the same threshold value is used to determine when internal battery charging occurs using a switched external battery (if the external power modification has been implemented).
    
    o If the load switch is used to control a cooling fan, the fan (load switch) will be turned on when the processor temperature reaches 45 °C, and will turn off when the temperature falls below 40 °C (if BAT X 0 has been applied).
    
    o The serial help text now describes the BAT command and the word “Master” has been replaced by “Source”.

--- Software/AVR128DA28/SignalSlinger/main.cpp
+++ Software/AVR128DA28/SignalSlinger/main.cpp
@@ -126,7 +126,7 @@ volatile time_t g_event_start_epoch = EEPROM_START_TIME_DEFAULT;
 volatile time_t g_event_finish_epoch = EEPROM_FINISH_TIME_DEFAULT;
 volatile bool g_event_enabled = EEPROM_EVENT_ENABLED_DEFAULT;                        /* indicates that the conditions for executing the event are set */
 volatile bool g_event_commenced = false;
-volatile float g_voltage_threshold = EEPROM_BATTERY_THRESHOLD_V;
+volatile float g_internal_voltage_low_threshold = EEPROM_INT_BATTERY_LOW_THRESHOLD_V;
 volatile float g_internal_bat_voltage = 0.;
 volatile float g_internal_bat_detected = false;
 volatile float g_external_voltage = 0.;
@@ -154,6 +154,7 @@ static volatile bool g_check_for_long_wakeup_press = true;
 static volatile bool g_device_wakeup_complete = false;
 static volatile bool g_charge_battery = false;
 extern bool g_enable_boost_regulator;
+static volatile bool g_turn_on_fan = false;
 
 
 #define NUMBER_OF_POLLED_ADC_CHANNELS 3
@@ -522,13 +523,6 @@ ISR(TCB0_INT_vect)
 						{
 							g_switch_closed_time = 0;
 							buttonReleased = true;
-					
-							if(g_send_clone_success_countdown || g_cloningInProgress) 
-							{
-								g_send_clone_success_countdown = 0;
-								g_cloningInProgress = false;
-								g_programming_msg_throttle = 0;
-							}
 						}
 					
 						longPressEnabled = true;
@@ -858,7 +852,10 @@ ISR(TCB0_INT_vect)
 				}
 				else if(g_adcChannelOrder[indexConversionInProcess] == ADCExternalBatteryVoltage)
 				{
-					g_external_voltage = (0.00725 * (float)g_lastConversionResult[indexConversionInProcess]) + 0.05;
+					if(!g_enable_external_battery_control || getExtBatLSEnable()) // Don't try to read a disconnected external battery
+					{
+						g_external_voltage = (0.00725 * (float)g_lastConversionResult[indexConversionInProcess]) + 0.05;
+					}
 				}
 				else if(g_adcChannelOrder[indexConversionInProcess] == ADCTemperature)
 				{
@@ -876,6 +873,21 @@ ISR(TCB0_INT_vect)
 						else
 						{
 							g_temperature_shutdown = g_temperature_shutdown ? !(g_processor_temperature < 80.) : g_processor_temperature > 85.;
+						}
+						
+						if(g_turn_on_fan)
+						{
+							if(g_processor_temperature <= FAN_TURN_OFF_TEMP)
+							{
+								g_turn_on_fan = false;
+							}
+						}
+						else
+						{
+							if(g_processor_temperature > FAN_TURN_ON_TEMP)
+							{
+								g_turn_on_fan = true;
+							}
 						}
 					}
 				}
@@ -961,7 +973,7 @@ int main(void)
 		
 	if(now == time(null))
 	{
-		LEDS.blink(LEDS_GREEN_OFF);
+		LEDS.blink(LEDS_GREEN_OFF); // Signal that the first attempt failed
 		LEDS.blink(LEDS_RED_OFF);
 		while((util_delay_ms(3000)) && (now == time(null)));
 	}
@@ -1004,17 +1016,19 @@ int main(void)
 				
 	buttonReleasedDuringStartup = false;
 	
+	/* Disable automatic ADC readings */
 	TCB0.INTCTRL = 0;   /* Capture or Timeout: disable interrupts */
 	TCB0.CTRLA = 0; /* Disable timer */
+	/* The external battery control load switch is initialized to ON, so we don't need to set it here */
 	g_internal_bat_voltage = readVoltage(ADCInternalBatteryVoltage);
 	g_external_voltage = readVoltage(ADCExternalBatteryVoltage);
 	
 	g_internal_bat_detected = (g_internal_bat_voltage > INT_BAT_PRESENT_VOLTAGE);
 
-	if(g_internal_bat_detected && (g_internal_bat_voltage < INT_BAT_CHARGE_THRESH_LOW) && (g_external_voltage > EXT_BAT_CHARGE_SUPPORT_THRESH_LOW)) // An external voltage is present and an internal battery is present & not fully charged
+	if(g_internal_bat_detected && (g_internal_bat_voltage < g_internal_voltage_low_threshold) && (g_enable_external_battery_control > 0)) // An internal battery is present & not fully charged & charging from an external battery is enabled
 	{
 		g_charge_battery = true;
-		if(g_enable_external_battery_control) setExtBatLoadSwitch(ON, INTERNAL_BATTERY_CHARGING);
+		setExtBatLoadSwitch(ON, INTERNAL_BATTERY_CHARGING);
 	}
 	g_restart_conversions = true;
 	TIMERB_init();
@@ -1060,7 +1074,7 @@ int main(void)
 			// Set internal battery charging
 			if((g_internal_bat_voltage > INT_BAT_PRESENT_VOLTAGE) && (g_external_voltage > EXT_BAT_CHARGE_SUPPORT_THRESH_LOW))
 			{
-				if(g_internal_bat_voltage < INT_BAT_CHARGE_THRESH_LOW) // An adequate external voltage is present and an internal battery is present & not fully charged
+				if(g_internal_bat_voltage < g_internal_voltage_low_threshold) // An adequate external voltage is present and an internal battery is present & not fully charged
 				{
 					g_charge_battery = true;
 				}
@@ -1074,7 +1088,27 @@ int main(void)
 				g_charge_battery = false;
 			}
 			
-			if(g_enable_external_battery_control) setExtBatLoadSwitch(g_charge_battery, INTERNAL_BATTERY_CHARGING);
+			if(g_enable_external_battery_control) 
+			{
+				setExtBatLoadSwitch(g_charge_battery, INTERNAL_BATTERY_CHARGING);
+			}
+			else
+			{
+				if(g_turn_on_fan)
+				{
+					if(!getExtBatLSEnable())
+					{
+						setExtBatLoadSwitch(ON, INITIALIZE_LS);
+					}
+				}
+				else
+				{
+					if(getExtBatLSEnable())
+					{
+						setExtBatLoadSwitch(OFF, INITIALIZE_LS);
+					}
+				}
+			}
 			
 			/********************************
 			 * Handle sleep
@@ -1154,12 +1188,14 @@ int main(void)
 							g_internal_bat_voltage = readVoltage(ADCInternalBatteryVoltage); // Throw out first result following sleep
 							g_external_voltage = readVoltage(ADCExternalBatteryVoltage);
 							system_sleep_config();
+							setExtBatLoadSwitch(RE_APPLY_LS_STATE);
 						
-							if(g_external_voltage > EXT_BAT_CHARGE_SUPPORT_THRESH_LOW)
+							if(g_enable_external_battery_control)
 							{
+
 								if(g_internal_bat_voltage > INT_BAT_PRESENT_VOLTAGE)
 								{
-									if(g_internal_bat_voltage < INT_BAT_CHARGE_THRESH_LOW) // An external voltage is present and an internal battery is present & not fully charged
+									if(g_internal_bat_voltage < g_internal_voltage_low_threshold) // An external voltage is present and an internal battery is present & not fully charged
 									{
 										g_charge_battery = true;
 									}
@@ -1252,7 +1288,15 @@ int main(void)
 			}
 			
 			if(g_handle_counted_presses)
-			{
+			{				
+				if(g_send_clone_success_countdown || g_cloningInProgress)
+				{
+					g_send_clone_success_countdown = 0;
+					g_cloningInProgress = false;
+					g_programming_msg_throttle = 0;
+					g_handle_counted_presses = 0; /* Throw out any keypresses that occurred while cloning or sending cloning success pattern */
+				}
+
 				if(g_isMaster)
 				{
 					if(g_handle_counted_presses == 5)
@@ -1546,7 +1590,7 @@ int main(void)
 			
 			if(g_device_enabled)
 			{
-				internal_bat_error = ((g_internal_bat_voltage > 0.0) && (g_internal_bat_voltage <= g_voltage_threshold));
+				internal_bat_error = (g_internal_bat_detected && (g_internal_bat_voltage <= g_internal_voltage_low_threshold));
 				external_pwr_error = (g_external_voltage <= EXT_BAT_PRESENT_VOLTAGE);
 				
 				if(g_charge_battery)
@@ -2563,7 +2607,7 @@ void __attribute__((optimize("O0"))) handleSerialBusMsgs()
 			}
 			break;
 
-			case SB_MESSAGE_VOLTS:
+			case SB_MESSAGE_BATTERY:
 			{
 				char txt[6];
 
@@ -2571,16 +2615,20 @@ void __attribute__((optimize("O0"))) handleSerialBusMsgs()
 				{
 					float v = atof(sb_buff->fields[SB_FIELD1]);
 
-					if((v >= 0.1) && (v <= 15.))
+					if((v >= INT_BAT_CHARGE_THRESH_LOW_MIN) && (v <= INT_BAT_CHARGE_THRESH_LOW_MAX))
 					{
-						g_voltage_threshold = v;
-						g_ee_mgr.updateEEPROMVar(Voltage_threshold, (void*)&g_voltage_threshold);
+						g_internal_voltage_low_threshold = v;
+						g_ee_mgr.updateEEPROMVar(Voltage_threshold, (void*)&g_internal_voltage_low_threshold);
 					}
 					else
 					{
-						sb_send_string((char*)"\nErr: 0.1 V < thresh < 15.0 V\n");
+						int16_t d1, d2;
+						uint16_t f1, f2;
+						float_to_parts_signed(INT_BAT_CHARGE_THRESH_LOW_MIN, &d1, &f1);
+						float_to_parts_signed(INT_BAT_CHARGE_THRESH_LOW_MAX, &d2, &f2);						
+  						sprintf(g_tempStr, "\nErr: %d.%u V < thresh < %d.%u V\n", d1, f1, d2, f2);
+						sb_send_string(g_tempStr);
 					}
-
 				}
 				else if(sb_buff->fields[SB_FIELD1][0] == 'X')
 				{
@@ -2606,24 +2654,28 @@ void __attribute__((optimize("O0"))) handleSerialBusMsgs()
 						updateStoredValue = true;
 					}
 					
-					if(updateStoredValue) g_ee_mgr.updateEEPROMVar(Enable_External_Battery_Control, (void*)&g_enable_external_battery_control);
+					if(updateStoredValue)
+					{
+						setExtBatLoadSwitch(OFF, INITIALIZE_LS);
+						g_ee_mgr.updateEEPROMVar(Enable_External_Battery_Control, (void*)&g_enable_external_battery_control);
+					}
 				}
-				else if(sb_buff->fields[SB_FIELD1][0] == 'B')
-				{
-					bool v = ((char)sb_buff->fields[SB_FIELD2][0] == '1');
-					g_enable_boost_regulator = v;					
-					g_ee_mgr.updateEEPROMVar(Enable_Boost_Regulator, (void*)&g_enable_boost_regulator);
-				}
+// 				else if(sb_buff->fields[SB_FIELD1][0] == 'B')
+// 				{
+// 					bool v = ((char)sb_buff->fields[SB_FIELD2][0] == '1');
+// 					g_enable_boost_regulator = v;					
+// 					g_ee_mgr.updateEEPROMVar(Enable_Boost_Regulator, (void*)&g_enable_boost_regulator);
+// 				}
 				
-				sprintf(g_tempStr, "\nBoost: %sabled\n", g_enable_boost_regulator ? "En":"Dis");
-				sb_send_string(g_tempStr);
+// 				sprintf(g_tempStr, "\nBoost: %sabled\n", g_enable_boost_regulator ? "En":"Dis");
+// 				sb_send_string(g_tempStr);
 
 				dtostrf(g_internal_bat_voltage, 5, 1, txt);
 				txt[5] = '\0';
-  				sprintf(g_tempStr, "Int. Bat =%s Volts\n", txt);
+  				sprintf(g_tempStr, "\nInt. Bat =%s Volts\n", txt);
  				sb_send_string(g_tempStr);
 
-				dtostrf(g_voltage_threshold, 5, 1, txt);
+				dtostrf(g_internal_voltage_low_threshold, 5, 1, txt);
 				txt[5] = '\0';
  				sprintf(g_tempStr, "thresh   =%s Volts\n", txt);
  				sb_send_string(g_tempStr);
@@ -3626,7 +3678,7 @@ void reportSettings(void)
 		sb_send_string(g_tempStr);
 	}
 
-	// Send the current settings header.
+	// Print the current settings header.
 	sb_send_string(TEXT_CURRENT_SETTINGS_TXT);
 
 	// Report the current system time.
@@ -3808,7 +3860,7 @@ void reportSettings(void)
 		{
 			sprintf(g_tempStr, "\n* WARNING: TRANSMIT DISABLED");
 			sb_send_string(g_tempStr);
-			sprintf(g_tempStr, "\n* Re-enable with BAT X 1\n");
+			sprintf(g_tempStr, "\n* Re-enable with > BAT X 1\n");
 			sb_send_string(g_tempStr);
 		}
 	}
