496d91bb26d13bba45911622da9c639ed7c2b497
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2026-03-27 12:12:58 -0400

    Bump SW_REVISION to 1.2e
    Update the SW_REVISION macro in Software/AVR128DA28/SignalSlinger/defs.h from "1.2d" to "1.2e" to reflect the new software revision for the next build/release.

--- Software/AVR128DA28/SignalSlinger/defs.h
+++ Software/AVR128DA28/SignalSlinger/defs.h
@@ -34,7 +34,7 @@
 
 /******************************************************
  * Set the text that gets displayed to the user */
-#define SW_REVISION "1.2d"
+#define SW_REVISION "1.2e"
 #define HW_TARGET_3_4
 // #define HW_TARGET_3_5
 
