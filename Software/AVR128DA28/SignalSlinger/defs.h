7311101f3bdd56de19e4275e0652e1e01bb49ee2
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2026-03-29 14:37:18 -0400

    Update SW_REVISION to 1.2g
    Bump the software revision string in Software/AVR128DA28/SignalSlinger/defs.h from "1.2f" to "1.2g" to reflect the new minor release/revision.
    
    In this version, multi-day events are fully equal to single-day events in terms of how they are supported.

--- Software/AVR128DA28/SignalSlinger/defs.h
+++ Software/AVR128DA28/SignalSlinger/defs.h
@@ -34,7 +34,7 @@
 
 /******************************************************
  * Set the text that gets displayed to the user */
-#define SW_REVISION "1.2f"
+#define SW_REVISION "1.2g"
 #define HW_TARGET_3_4
 // #define HW_TARGET_3_5
 
