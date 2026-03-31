2f4c4c7f7e68f4e963d118c7b895493a4e76fce5
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2026-03-31 19:45:51 -0400

    Reformat adc.cpp whitespace and comments
    Apply non-functional formatting across Software/AVR128DA28/SignalSlinger/src/adc.cpp: normalize spacing and indentation, align inline comments, remove trailing whitespace, and tidy blank lines. No logic or behavior changes to ADC functionality—only stylistic edits for readability and consistency.

--- Software/AVR128DA28/SignalSlinger/src/adc.cpp
+++ Software/AVR128DA28/SignalSlinger/src/adc.cpp
@@ -28,11 +28,11 @@
 #include <stdbool.h>
 #include <driver_init.h>
 #include <compiler.h>
-	
+
 static void PORT_init(void);
 static void VREF0_init(void);
 static void ADC0_init(bool freerun);
-	
+
 ADC_Init_t g_adc_initialization = ADC_NOT_INITIALIZED;
 
 void ADC0_setADCChannel(ADC_Active_Channel_t chan)
@@ -45,60 +45,60 @@ void ADC0_setADCChannel(ADC_Active_Channel_t chan)
 			{
 				ADC0_SYSTEM_init(SINGLE_CONVERSION);
 			}
-			
+
 			ADC0.MUXPOS = ADC_MUXPOS_AIN0_gc;
 		}
 		break;
-		
+
 		case ADCExternalBatteryVoltage:
 		{
 			if(g_adc_initialization != ADC_SINGLE_CONVERSION_INITIALIZED)
 			{
 				ADC0_SYSTEM_init(SINGLE_CONVERSION);
 			}
-			
+
 			ADC0.MUXPOS = ADC_MUXPOS_AIN1_gc;
 		}
 		break;
-		
+
 		case ADC12VRegulatedVoltage:
 		{
 			if(g_adc_initialization != ADC_SINGLE_CONVERSION_INITIALIZED)
 			{
 				ADC0_SYSTEM_init(SINGLE_CONVERSION);
 			}
-			
+
 			ADC0.MUXPOS = ADC_MUXPOS_AIN4_gc;
 		}
 		break;
-		
+
 		case ADCTXAdjustableVoltage:
 		{
 			if(g_adc_initialization != ADC_SINGLE_CONVERSION_INITIALIZED)
 			{
 				ADC0_SYSTEM_init(SINGLE_CONVERSION);
 			}
-			
+
 			ADC0.MUXPOS = ADC_MUXPOS_AIN5_gc;
 		}
 		break;
-		
+
 		case ADCTemperature:
 		{
 			if(g_adc_initialization != ADC_SINGLE_CONVERSION_INITIALIZED)
 			{
 				ADC0_SYSTEM_init(SINGLE_CONVERSION);
 			}
-			
+
 			ADC0.MUXPOS = ADC_MUXPOS_TEMPSENSE_gc;
 		}
 		break;
-		
+
 		default:
 		{
 			ADC0_SYSTEM_shutdown();
 		}
-		break;	
+		break;
 	}
 }
 
@@ -106,7 +106,7 @@ void ADC0_startConversion(void)
 {
 	if(g_adc_initialization != ADC_NOT_INITIALIZED)
 	{
-		ADC0.INTCTRL = 0x00; /* Disable interrupt */
+		ADC0.INTCTRL = 0x00;          /* Disable interrupt */
 		ADC0.COMMAND = ADC_STCONV_bm; /* Start conversion */
 	}
 }
@@ -119,87 +119,85 @@ bool ADC0_conversionDone(void)
 
 int ADC0_read()
 {
-	return ADC0.RES; 	/* Reading the result also clears the interrupt flag */
+	return ADC0.RES; /* Reading the result also clears the interrupt flag */
 }
-
 
 float readVoltage(ADC_Active_Channel_t chan)
 {
 	uint16_t adc_reading;
 	uint32_t wait = 10000;
 	float voltage = 0;
-	
+
 	adc_reading = ADC0.RES;
- 	ADC0_setADCChannel(chan);
+	ADC0_setADCChannel(chan);
 	ADC0_startConversion();
-	
-	while((!ADC0_conversionDone()) && wait--);
-	
+
+	while((!ADC0_conversionDone()) && wait--)
+		;
+
 	if(wait)
 	{
 		adc_reading = ADC0.RES;
 		voltage = (0.00725 * (float)adc_reading) + 0.05;
 	}
-	
-	return(voltage);
-}
 
+	return (voltage);
+}
 
 bool isValidTemp(float temperatureC)
 {
-	return((temperatureC > MINIMUM_VALID_TEMP) && (temperatureC < MAXIMUM_VALID_TEMP));
+	return ((temperatureC > MINIMUM_VALID_TEMP) && (temperatureC < MAXIMUM_VALID_TEMP));
 }
 
 float temperatureCfromADC(uint16_t adc_reading)
 {
 	uint16_t sigrow_offset = SIGROW.TEMPSENSE1; // Read unsigned value from signature row
-	uint16_t sigrow_slope = SIGROW.TEMPSENSE0; // Read unsigned value from signature row
+	uint16_t sigrow_slope = SIGROW.TEMPSENSE0;  // Read unsigned value from signature row
 	float temperature_in_C = -273.15;
 
 	uint32_t temp = sigrow_offset - adc_reading;
 	temp *= sigrow_slope; // Result will overflow 16-bit variable
-	temp += 0x0800; // Add 4096/2 to get correct rounding on division below
-	temp >>= 12; // Round off to nearest degree in Kelvin, by dividing with 2^12 (4096)
+	temp += 0x0800;       // Add 4096/2 to get correct rounding on division below
+	temp >>= 12;          // Round off to nearest degree in Kelvin, by dividing with 2^12 (4096)
 	temperature_in_C += (float)temp;
-	
-	return(temperature_in_C);
-}
 
+	return (temperature_in_C);
+}
 
 static void PORT_init(void)
 {
 	/* Disable interrupt and digital input buffer on PD0 */
 	PORTD.PIN0CTRL &= ~PORT_ISC_gm;
 	PORTD.PIN0CTRL |= PORT_ISC_INPUT_DISABLE_gc;
-	
+
 	/* Disable pull-up resistor */
 	PORTD.PIN0CTRL &= ~PORT_PULLUPEN_bm;
 }
 
 static void VREF0_init(void)
 {
-	VREF.ADC0REF = VREF_REFSEL_2V048_gc;  /* Internal 2.048V reference */
+	VREF.ADC0REF = VREF_REFSEL_2V048_gc; /* Internal 2.048V reference */
 }
 
 static void ADC0_init(bool freerun)
 {
-	ADC0.CTRLC = ADC_PRESC_DIV64_gc;   /* CLK_PER divided by 4 => 24096 sps */
-	
+	ADC0.CTRLC = ADC_PRESC_DIV64_gc; /* CLK_PER divided by 4 => 24096 sps */
+
 	if(freerun)
 	{
-		ADC0.CTRLA = ADC_ENABLE_bm /* ADC Enable: enabled */
-		| ADC_RESSEL_12BIT_gc      /* 12-bit mode */
-		| ADC_FREERUN_bm;          /* Enable Free-Run mode */
-		
+		ADC0.CTRLA = ADC_ENABLE_bm         /* ADC Enable: enabled */
+		             | ADC_RESSEL_12BIT_gc /* 12-bit mode */
+		             | ADC_FREERUN_bm;     /* Enable Free-Run mode */
+
 		ADC0.INTCTRL = 0x01; /* Enable interrupt */
-		
+
 		ADC0.COMMAND = ADC_STCONV_bm; /* Start conversion */
 		g_adc_initialization = ADC_FREE_RUN_INITIALIZED;
 	}
 	else
 	{
-		ADC0.CTRLA |= ADC_ENABLE_bm;  /* ADC Enable: enabled; 12-bit mode is default */
-		ADC0.INTCTRL = 0x00; /* Disable interrupt */
+		ADC0.CTRLA |= ADC_ENABLE_bm; /* ADC Enable: enabled; 12-bit mode is default */
+		ADC0.INTCTRL = 0x00;         /* Disable interrupt */
 		g_adc_initialization = ADC_SINGLE_CONVERSION_INITIALIZED;
 	}
 }
@@ -213,7 +211,7 @@ void ADC0_SYSTEM_init(bool freerun)
 
 void ADC0_SYSTEM_shutdown(void)
 {
-	ADC0.INTCTRL = 0x00; /* Disable interrupt */
+	ADC0.INTCTRL = 0x00;              /* Disable interrupt */
 	ADC0.CTRLA = ADC_RESSEL_12BIT_gc; /* Turn off ADC leaving 12-bit resolution set */
 	g_adc_initialization = ADC_NOT_INITIALIZED;
 }
