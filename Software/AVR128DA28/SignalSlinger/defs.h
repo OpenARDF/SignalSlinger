daf26f8d5cea24b6b43208915fc60ea12e90833e
OpenARDF <66529806+OpenARDF@users.noreply.github.com>
2026-04-07 09:41:35 -0400

    Bump software revision to 1.2q

--- Software/AVR128DA28/SignalSlinger/defs.h
+++ Software/AVR128DA28/SignalSlinger/defs.h
@@ -34,7 +34,7 @@
 
 /******************************************************
  * Set the text that gets displayed to the user */
-#define SW_REVISION "1.2p"
+#define SW_REVISION "1.2q"
 #define HW_TARGET_3_4
 // #define HW_TARGET_3_5
 
